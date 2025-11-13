f48f03e882cac2f9897e2632a5754621a968d444
Richard Fazin Souza <richardfacinsouza@hotmail.com>
2025-11-13 19:55:25 -0300

    update

---
+++ trabalho_3/instancias/README.txt
@@ -0,0 +1,12 @@
+Instances retrieved from
+http://www.al.cm.is.nagoya-u.ac.jp/~yagiura/gap/
+
+The format of the instances is the following:
+
+number of agents (m), number of jobs (n)
+ for each agent i (i=1,...,m) in turn:
+     cost of allocating job j to agent i (j=1,...,n)
+ for each agent i (i=1,...,m) in turn:
+     resource consumed in allocating job j to agent i (j=1,...,n)
+ resource capacity of agent i (i=1,...,m)
+
---
+++ trabalho_3/item2.py
@@ -0,0 +1,62 @@
+from pyscipopt import Model
+
+with open("instancias/c20400", "r") as f:
+    data = list(map(float, f.read().split()))
+
+m = int(data[0])
+
+n = int(data[1])
+
+index = 2
+
+c = []
+
+for i in range(m):
+    c.append(data[index:index + n])
+    index += n
+
+a = []
+for i in range(m):
+    a.append(data[index:index + n])
+    index += n
+
+b = data[index:index + m]
+
+model = Model()
+
+x = {}
+
+for i in range(m):
+    for j in range(n):
+        x[i, j] = model.addVar(vtype="B", name=f"x({i},{j})")
+
+model.setObjective(
+    sum(
+        c[i][j] * x[i, j] for i in range(m) 
+            for j in range(n)
+    ),
+    "minimize"
+)
+
+for j in range(n):
+    model.addCons(sum(x[i, j] for i in range(m)) == 1)
+
+for i in range(m):
+    model.addCons(sum(a[i][j] * x[i, j] for j in range(n)) <= b[i])
+
+# Restrição para deixar mais forte
+for i in range(m):
+    for j in range(n):
+        if a[i][j] > b[i]:
+            model.addCons(x[i, j] == 0)
+#-----------------------------------
+
+#comando pedido no trabalho
+model.setParam("presolving/maxrounds", 0)
+model.setParam("separating/maxrounds", 0)
+
+model.setParam("limits/time", 300)
+
+model.optimize()
+
+print(model.getObjVal())
